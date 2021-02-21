a40cf8ead1dd068282e125a818ccb8519c641864
GPedro <gpedro178@gmail.com>
2021-02-20 23:11:28 -0300

    Drop Wrong Dates

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
+###############
+# En este trabajo se busca analizar la base de datos de precios de los 
+# combustibles de Argentina 2017-2021, en particular de la nafta súper, 
+# con el objetivo de determinar un índice de inflación.
+# El dataset empleado tiene fecha de actualización 15/02/2021.
+###############
+
 import pandas as pd
-#import datetime as dtime
-#from datetime import datetime
 
 archivoCSV = pd.read_csv("Precio-Nafta/precios-historicos.csv")
 dataFrame = pd.DataFrame(archivoCSV)
@@ -22,13 +27,12 @@ df_YPF["fecha_vigencia"] = [fecha[0:10] for fecha \
 # .describe() en formato decimal redondeado a 2 decimales
 
 # .describe() con la función "max" mostró que hay uno o más valores anormales
-# en la columna"precios"
+# en la columna "precios"
 
 # Buscando todos los valores anormales, en este caso cualquier valor "precio"
-# por encima de 100
-
+# por encima de 100 se considera anormal
 #print(df_YPF[df_YPF["precio"] > 100])
-#indiceError = (df_YPF[df_YPF["precio"] > 100]).index
+#indiceErrorPrecios = (df_YPF[df_YPF["precio"] > 100]).index
 
 # Función para corregir el error decimal en la tabla
 def div_100(x) :
@@ -39,22 +43,19 @@ def div_100(x) :
 # Reemplazando valores erroneos con la función
 df_YPF["precio"] = [div_100(x) for x in df_YPF["precio"]]
 
-# Revisando corrección (requiere remover "#" de la línea 30)
-#print(df_YPF.loc[indiceError])
+# Revisando corrección (requiere remover "#" de la línea 35)
+#print(df_YPF.loc[indiceErrorPrecios])
 
-df_YPF = df_YPF[["precio","fecha_vigencia"]]
+df_YPF = df_YPF[["precio","fecha_vigencia","producto"]]
 
-# Traduciendo fecha para Python con datetime
-formatoFecha = "%d/%m/%Y"
-#df_YPF["fecha_vigencia"] = [datetime.strptime(x, formatoFecha) for x \
-#    in df_YPF["fecha_vigencia"]]
-
+# Traduciendo fecha para Python con to_datetime de Pandas
 df_YPF["fecha_vigencia"] = pd.to_datetime(df_YPF["fecha_vigencia"])
-#print(df_YPF)
 
-#print(df_YPF["fecha_vigencia"].nlargest(10))
+# Eliminando fechas inferiores a 2017-01-01 y superiores a 2021-02-15 
+indiceErrorFechas = (df_YPF[ \
+    (df_YPF["fecha_vigencia"] < "2017-01-01") | \
+    (df_YPF["fecha_vigencia"] > "2021-02-15") \
+        ]).index
 
-print(df_YPF[(df_YPF["fecha_vigencia"] > "2021-12-31") | (df_YPF["fecha_vigencia"] < "2016-01-01")].sort_values(by=["fecha_vigencia"]))
+df_YPF = df_YPF.drop(indiceErrorFechas)
 
-#print(df_YPF[(df_YPF["fecha_vigencia"]<datetime(year=2016, month=5, day=1)) & \
-#    (df_YPF["fecha_vigencia"]>datetime(year=2016, month=1, day=1))])
